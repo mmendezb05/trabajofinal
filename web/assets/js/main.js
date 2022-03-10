160c83696ecd654d0c961d7b71910554bcd5fe95
mmendezb05 <91590831+mmendezb05@users.noreply.github.com>
2022-03-10 07:57:52 +0100

    Add files via upload

---
+++ web/assets/js/main.js
@@ -0,0 +1,47 @@
+// Desplegables del Homepage.html
+
+function DropDown(el) {
+    this.dd = el;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.drop li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+}
+
+DropDown.prototype = {
+    initEvents: function () {
+        var obj = this;
+        obj.dd.on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $(this).toggleClass('active');
+        });
+        obj.opts.on('click', function () {
+            var opt = $(this);
+            obj.val = opt.text();
+            obj.index = opt.index();
+            obj.placeholder.text(obj.val);
+            opt.siblings().removeClass('selected');
+            opt.filter(':contains("' + obj.val + '")').addClass('selected');
+        }).change();
+    },
+    getValue: function () {
+        return this.val;
+    },
+    getIndex: function () {
+        return this.index;
+    }
+};
+
+$(function () {
+    // Crear una variable para cada desplegable
+    var dd1 = new DropDown($('#genero'));
+    var dd2 = new DropDown($('#edad'));
+    $(document).click(function () {
+        // Cerrar el desplegable al hacer click en un elemento
+        $('.wrap-drop').removeClass('active');
+    });
+});
+
+
